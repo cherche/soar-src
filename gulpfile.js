7a1556926b852e202d8bd2e0443f77451b5e4aec
Ryan Nguyen <contactryannguyen@gmail.com>
2021-01-06 20:06:23 -0500

    Removed unnecessary packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 const gulp = require('gulp')
 const resizer = require('gulp-images-resizer');
-const debug = require('gulp-debug')
 
-gulp.task('binaries', function () {
-  return gulp
-    .src([
-      'img/**/*.jpg',
-      '!img/thumbnail/**/*.jpg'
-    ])
-    .pipe(debug())
-    .pipe(gulp.dest('binaries.txt'))
-})
-
-gulp.task('gen-thumbnails', function () {
+gulp.task('thumbnails', function () {
   return gulp
     .src([
       'img/**/*.jpg',
