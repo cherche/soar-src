e6d4bc1af924129e32ce86b8c2000c9f06e4759c
Ryan Nguyen <contactryannguyen@gmail.com>
2021-01-06 20:53:29 -0500

    Added less transpiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp')
-const resizer = require('gulp-images-resizer');
+const resizer = require('gulp-images-resizer')
+const less = require('gulp-less')
+const path = require('path')
 
 function thumbnails () {
   return gulp.src([
@@ -14,4 +16,13 @@ function thumbnails () {
     .pipe(gulp.dest('img/thumbnail/'))
 }
 
+function styles () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(gulp.dest('./css'))
+}
+
 exports.thumbnails = thumbnails
+exports.styles = styles
