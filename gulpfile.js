554edb153154d6110b7ec05c694e366b8f1ce649
Ryan Nguyen <contactryannguyen@gmail.com>
2021-01-07 22:11:51 -0500

    Set up gulp tasks for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,55 @@
 const gulp = require('gulp')
-const resizer = require('gulp-images-resizer')
+
+const gulpPug = require('gulp-pug')
 const gulpLess = require('gulp-less')
 const path = require('path')
+const resizer = require('gulp-images-resizer')
+
 const cleanCSS = require('gulp-clean-css')
 const rename = require('gulp-rename')
-const gulpPug = require('gulp-pug')
 
-gulp.task('dev-thumbnails', function () {
+gulp.task('views-dev', function () {
+  return gulp.src('src/views/*.pug')
+    .pipe(gulpPug({
+      doctype: 'html'
+    }))
+    .pipe(gulp.dest('./dev'))
+})
+
+gulp.task('styles-dev', function () {
   return gulp.src([
-      'assets/img/**/*.jpg',
-      '!assets/img/exec/**/*.jpg',
-      '!assets.img/thumbnail/**/*.jpg'
+    './src/less/**/*.less',
+    '!./src/less/main.less'
+  ])
+    .pipe(gulpLess({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(gulp.dest('./dev/css'))
+})
+
+gulp.task('scripts-dev', function () {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(gulp.dest('./dev/js'))
+})
+
+gulp.task('lib-dev', function () {
+  return gulp.src('lib/**/*')
+    .pipe(gulp.dest('dev/'))
+})
+
+gulp.task('assets-dev', function () {
+  return gulp.src([
+    'assets/**/*',
+    '!assets.img/thumbnail/**/*.jpg'
+  ])
+    .pipe(gulp.dest('dev/'))
+})
+
+gulp.task('thumbnails-dev', function () {
+  return gulp.src([
+      'dev/img/**/*.jpg',
+      '!dev/img/exec/**/*.jpg',
+      '!dev.img/thumbnail/**/*.jpg'
     ])
     .pipe(resizer({
       format: 'jpg',
@@ -19,36 +58,27 @@ gulp.task('dev-thumbnails', function () {
     .pipe(gulp.dest('dev/img/thumbnail/'))
 })
 
-gulp.task('dev-less', function () {
-  return gulp.src('./src/less/**/*.less')
-    .pipe(gulpLess({
-      paths: [path.join(__dirname, 'less', 'includes')]
-    }))
-    .pipe(gulp.dest('./dev/css'))
+// One-time gulp task to get the entire dev environment set up
+gulp.task('dev-build', gulp.parallel(
+  'views-dev',
+  'styles-dev',
+  'scripts-dev',
+  'lib-dev',
+  gulp.series('assets-dev', 'thumbnails-dev')
+))
+
+// Continuous gulp task to make development easier
+gulp.task('dev-mode', function () {
+  gulp.watch('./src/views/**/*.pug', gulp.series('views-dev'))
+  gulp.watch('./src/less/**/*.less', gulp.series('styles-dev'))
+  gulp.watch('./src/js/**/*.js', gulp.series('scripts-dev'))
 })
 
-gulp.task('build-css', function () {
+// START OF BUILD TASKS
+
+gulp.task('styles-build', function () {
   return gulp.src('css/main.css')
     .pipe(cleanCSS())
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('css'))
-})
-
-function styles () {
-  return gulp.series(['less', 'build-css'])
-}
-
-gulp.task('dev-pug', function () {
-  return gulp.src('src/views/*.pug')
-    .pipe(gulpPug({
-      doctype: 'html',
-      pretty: true
-    }))
-    .pipe(gulp.dest('./dev'))
-})
-
-exports.styles = styles
-gulp.task('dev', function () {
-  gulp.watch('./src/less/**/*.less', gulp.series('dev-less'))
-  gulp.watch('./src/views/**/*.pug', gulp.series('dev-pug'))
 })
